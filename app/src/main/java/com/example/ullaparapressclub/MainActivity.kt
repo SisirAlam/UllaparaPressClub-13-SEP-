d135cc74e8f162590cb49d7b4c88fb77f102c221
SisirAlam <sisir.info@gmail.com>
2026-09-20 01:30:37 +0600

    chore: ignore gradle build cache files

--- app/src/main/java/com/example/ullaparapressclub/MainActivity.kt
+++ app/src/main/java/com/example/ullaparapressclub/MainActivity.kt
@@ -1,17 +1,20 @@
-package com.ullaparapressclub.app
+package com.example.ullaparapressclub
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
+import com.example.ullaparapressclub.ui.MainScreen
+import com.example.ullaparapressclub.ui.theme.UllaparaPressClubTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Text("Ullapara Press Club")
+            UllaparaPressClubTheme {
+                MainScreen()
+            }
         }
     }
 }
